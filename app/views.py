f984ac843ad0f93f87f63087ac7dbe167459b2aa
Arjen Dijkstra <dijkstra.arjen@gmail.com>
2015-11-17 16:27:04 +0100

    Error templates

--- app/views.py
+++ app/views.py
@@ -112,3 +112,14 @@ def edit_profile():
         form.about_me.data = g.user.about_me
 
     return render_template("edit_profile.html", form=form)
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
